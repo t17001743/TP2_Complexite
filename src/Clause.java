fd34b9df35cd753cf16440ada93d3d4a22657a98
t17001743 <samantha.thiel@etu.univ-amu.fr>
2020-12-21 22:44:16 +0100

    Mini projet 1 svg

--- src/Clause.java
+++ src/Clause.java
@@ -1,5 +1,3 @@
-package Mini_projet_1;
-
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -9,7 +7,7 @@ public class Clause {
     private String fichierFormule, fichierAffectation;  //les deux fichiers .cnf
     private ArrayList<Boolean> tabValeurs;  //liste des valeurs affectées aux variables
     private ArrayList<ArrayList<Integer>> tabClauses;  //liste des clauses de la formule
-    private int nbVariables, nbClauses;
+    private int nbVariables, nbClauses;  //nombre de variables et nombre de clauses de la formule
 
 
     /** Constructeur initialisant les paramètres et lisant les deux fichiers .cnf en stockant les données dans des ArrayList
@@ -27,16 +25,26 @@ public class Clause {
     }
 
 
+    /**
+     * @param i le numéro de la variable
+     * @return la valeur de la variable xi
+     * @throws Exception
+     */
     public boolean getValX(int i) throws Exception {
         if(1 <= i && i <= nbVariables) return tabValeurs.get(i);
-        if(1 <= -i && -i <= nbVariables) return !tabValeurs.get(-i);
+        if(1 <= -i && -i <= nbVariables) return !tabValeurs.get(-i);  //si la valeur a un "-" devant
         throw new Exception("Mauvais indice de valeur");
     }
 
 
+    /**
+     * Changer la valeur de la variable xi
+     * @param i le numéro de la variable
+     * @throws Exception
+     */
     public void setValX(int i) throws Exception{
         if(1 <= i && i <= nbVariables) tabValeurs.set(i, true);
-        else if (1 <= -i && -i <= nbVariables) tabValeurs.set(-i, false);
+        else if (1 <= -i && -i <= nbVariables) tabValeurs.set(-i, false);  //si la valeur a un "-" devant
         else throw new Exception("Mauvais indice de valeur");
     }
 
@@ -48,34 +56,40 @@ public class Clause {
     public void readFileFormule() throws Exception {
         Scanner lectureFichierFormule = new Scanner(new FileReader(fichierFormule));
 
-        // parcourir le fichier et le stocker dans le tableau de données
+        //parcourir la première ligne du fichier et stocker ses données dans un tableau
         if(lectureFichierFormule.hasNextLine()) {
             String ligne = lectureFichierFormule.nextLine();
-            String[] parts = ligne.split(" ");
+            String[] parts = ligne.split(" ");  //on stocke les données lues dans un tableau
 
+            //erreur si le fichier .cnf n'est pas correctement écrit
             if(!parts[0].equals("p") || !parts[1].equals("cnf")){
                 throw new Exception("Le fichier .cnf est incorrect.");
             }
+
+            //le nombre de variables se trouve juste après le "p cnf" sur la première ligne du fichier
             nbVariables = Integer.parseInt(parts[2]);
+            //le nombre de clauses de la formule est la dernière valeur de la première ligne
             nbClauses = Integer.parseInt(parts[3]);
 
-            // crée la liste des variables
+            //on crée la liste des variables de la formule
             for (int i=0; i <= nbVariables; i++)
                 tabValeurs.add(false);
-
-            // pour tabClauses pas besoin, on va faire des add()
         }
         else throw new Exception("Fichier trop court");
 
+        //parcours des lignes du fichier pour remplir la liste des clauses
         while (lectureFichierFormule.hasNextLine()) {
             String ligne = lectureFichierFormule.nextLine();
-            String[] parts = ligne.split(" ");
+            String[] parts = ligne.split(" ");  //on stocke les données lues dans un tableau
 
             ArrayList<Integer> ligneClause = new ArrayList<Integer>();
 
+            //ajout de l'ensemble des variables appartenant à une même clause
             for(String part : parts) {
                 ligneClause.add(Integer.parseInt(part));
             }
+
+            //ajout de la clause lue à la liste de clauses de la formule
             if(ligneClause.size() > 0){
                 tabClauses.add(ligneClause);
             }
@@ -90,10 +104,12 @@ public class Clause {
     public void readFileAffectation() throws Exception {
         Scanner lectureFichierAffectation = new Scanner(new FileReader(fichierAffectation));
 
+        //lecture des données du fichier d'affectations
         if(lectureFichierAffectation.hasNextLine()) {
             String ligne = lectureFichierAffectation.nextLine();
-            String[] parts = ligne.split(" ");
+            String[] parts = ligne.split(" ");  //on stocke les données lues dans un tableau
 
+            //on affecte chaque valeur lue à la variable correspondante dans sa formule
             for(String part : parts) {
                 int k = Integer.parseInt(part);
                 setValX(k);
@@ -108,19 +124,23 @@ public class Clause {
      * @throws Exception
      */
     public boolean evaluer() throws Exception {
-        boolean resGlobal = true;
+        boolean resGlobal = true;   //le résultat final du calcul de la formule
 
+        //pour chaque clause de la formule
         for(ArrayList<Integer> ligneClause : tabClauses){
-            boolean resLigne = false;
+            boolean resLigne = false;  //résultat du calcul de la clause
 
             for(int k : ligneClause){
                 if(k == 0) break;
+                //"OU" entre chaque membre d'une même clause
                 resLigne = resLigne || getValX(k);
             }
 
+            //"ET" entre chaque clause
             resGlobal = resGlobal && resLigne;
         }
 
+        //renvoi du résultat final
         return resGlobal;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
-package Mini_projet_1;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        //chemin des fichiers
-        String filePath = "src/Mini_projet_1/";
-        Clause clause = new Clause(filePath+"formule1.cnf", filePath+"affectationVariables1.cnf");
+        String filePath = "./src/";  //chemin des fichiers
+        Clause clause = new Clause("formule1.cnf", "affectationVariables1.cnf");
 
         //résultat du calcul de la formule avec l'affectation donnée en entrée
         boolean resultat = clause.evaluer();
 
+        System.out.println();
         System.out.println("----------------------Vérificateur déterministe pour SAT----------------------");
         System.out.println();
         System.out.println("L'affectation satisfait-elle la formule ?");
@@ -19,5 +17,6 @@ public class Main {
         if(!resultat) System.out.println("Réponse : NON");
         //sinon, la formule est vraie et donc l'affectation la vérifie
         else System.out.println("Réponse : OUI");
+        System.out.println();
     }
 }
